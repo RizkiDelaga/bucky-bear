8377672bac4bf25b8d1721dff0768ca7d17fd702
RizkiDelaga <72425333+RizkiDelaga@users.noreply.github.com>
2023-03-21 17:03:47 +0700

    Belajar grid

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,3 +1,4 @@
+import { Container, Grid } from '@mui/material';
 import React, { Fragment } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../Components/Header/Header';
@@ -12,8 +13,8 @@ function Login() {
 
   return (
     <Fragment>
-      <Header />
-      <h1>Ini halaman Login</h1>
+      {/* <Header />
+      <h1>Ini halaman Login</h1> */}
       <button
         onClick={() => {
           navigate('/');
@@ -22,7 +23,7 @@ function Login() {
         Balik Ke Homepage
       </button>
 
-      <br />
+      {/* <br />
       <hr />
       <div className="flex-div">
         <div className="flex1">
@@ -45,21 +46,46 @@ function Login() {
             <h2>Lorem, ipsum.</h2>
           </div>
         </div>
-      </div>
+      </div> */}
 
       <div style={{ display: 'flex' }}></div>
 
-      <div className="card-example">
-        <h1>Lorem ipsum</h1>
-        <h3>12</h3>
-      </div>
+      <Container>
+        <Grid container spacing={5}>
+          <Grid item md xl={4} xs={6} className="centerGrid">
+            <div className="card-example">
+              <h1>Lorem ipsum</h1>
+              <h3>12</h3>
+            </div>
+          </Grid>
+          <Grid item md xl={4} xs={6} className="centerGrid">
+            <div className="card-example">
+              <h1>Lorem ipsum</h1>
+              <h3>12</h3>
+            </div>
+          </Grid>
+          <Grid item md xl={4} xs={6} className="centerGrid">
+            <div className="card-example">
+              <h1>Lorem ipsum</h1>
+              <h3>12</h3>
+            </div>
+          </Grid>
+          <Grid item md xl={4} xs={6} className="centerGrid">
+            <div className="card-example">
+              <h1>Lorem ipsum</h1>
+              <h3>12</h3>
+            </div>
+          </Grid>
+          <Grid item md xl={4} xs={6} className="centerGrid">
+            <div className="card-example">
+              <h1>Lorem ipsum</h1>
+              <h3>12</h3>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
 
-      <div style={{ marginTop: '300px' }}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id consectetur, eius libero fugit esse mollitia
-          incidunt ipsam labore est alias impedit perferendis a velit neque fugiat placeat nemo unde natus!
-        </p>
-      </div>
+      
     </Fragment>
   );
 }
